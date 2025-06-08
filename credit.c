87e1aca7130a61a687c5c65575e630d48ef1fac3
CristianMB2255 <84812535+CristianMB2255@users.noreply.github.com>
2025-06-08 18:50:02 -0300

    Update credit.c

--- credit.c
+++ credit.c
@@ -4,31 +4,40 @@
 #include <string.h>
 
 long get_credit_number();
-int get_card_info(char *numberStr, int length, char **cardType);
-int validate_card(char *numberStr, int length);
+void get_card_type(char *number, int numberLength, char **cardType);
+int luhn_checksum(char *number, int numberLength);
 
 int main(void)
 {
-    int return_value = 1;
-    char numberStr[17];
+    char number[17];
     char *cardType = NULL;
 
-    // Get the credit card number
-    long number = get_credit_number();
-    sprintf(numberStr, "%ld", number);
+    // Get the credit card number & length
+    sprintf(number, "%ld", get_credit_number());
+    int numberLength = strlen(number);
 
-    // Get the length of the number
-    int length = strlen(numberStr);
+    // Get card info
+    get_card_type(number, numberLength, &cardType);
 
-    // Get the type of the card and validate it
-    if (get_card_info(numberStr, length, &cardType) == 0 || validate_card(numberStr, length) == 1)
+    if (strcmp(cardType, "INVALID") == 0)
     {
         printf("INVALID\n");
         return 0;
     }
 
-    // If correct, print the card type
-    printf("%s\n", cardType);
+    // Validate the card w/ Hans Peter Luhn algorithm
+    int sum = luhn_checksum(number, numberLength);
+
+    // Print the card type or invalid
+    if (sum % 10 == 0)
+    {
+        printf("%s\n", cardType);
+    }
+
+    else
+    {
+        printf("INVALID\n");
+    }
     return 0;
 }
 
@@ -46,46 +55,47 @@ long get_credit_number()
 }
 
 // Get the type of the credit card
-int get_card_info(char *numberStr, int length, char **cardType)
+void get_card_type(char *number, int numberLength, char **cardType)
 {
-    // AMEX: 15; 34, 37
-    // Master: 16; 51, 52, ..., 55
-    // VISA: 13 or 16; 4
+    // AMEX: 15 length; starts with: 34, 37
+    // Master: 16 length; starts with: 51, 52, ..., 55
+    // VISA: 13 or 16 length; starts with: 4
 
-    if (length == 15 && numberStr[0] == '3' && strchr("47", numberStr[1]))
-    {
+    if (numberLength == 15 && number[0] == '3' && strchr("47", number[1]))
         *cardType = "AMEX";
-        return 1;
-    }
-    if ((length == 13 || length == 16) && numberStr[0] == '4')
-    {
-        *cardType = "VISA";
-        return 1;
-    }
-    if (length == 16 && numberStr[0] == '5' && strchr("12345", numberStr[1]))
-    {
-        *cardType = "MASTERCARD";
-        return 1;
-    }
 
-    // Return if the card isn't in a valid format
-    return 0;
+    else if ((numberLength == 13 || numberLength == 16) && number[0] == '4')
+        *cardType = "VISA";
+
+    else if (numberLength == 16 && number[0] == '5' && strchr("12345", number[1]))
+        *cardType = "MASTERCARD";
+
+    else
+        *cardType = "INVALID";
 }
 
 // Validate the credit card digits
-int validate_card(char *numberStr, int length)
+int luhn_checksum(char *number, int numberLength)
 {
     int sum = 0;
-    for (int i = length - 1; i >= 0; i--)
+
+    // Iterate through number
+    for (int i = 0; i < numberLength; i++)
     {
-        int digit = numberStr[i] - '0';
-        if ((length - i) % 2 == 0)
+        int digit = number[i] - '0';
+
+        // If even
+        // Double the digit and sum the product's digits
+        if ((numberLength - i) % 2 == 0)
             digit = digit * 2;
 
         if (digit > 9)
             sum += digit - 9;
+
+        // If odd
+        // Sum the digit
         else
             sum += digit;
     }
-    return sum % 10 != 0;
+    return sum;
 }
